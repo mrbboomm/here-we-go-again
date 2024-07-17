b5caf2e9de34a9f20663f7b8a9d12ea9ef9b7a6f
thitiwut.t <thitiwut.t@muze.co.th>
2024-07-17 16:53:40 +0700

    inti: kafka

---
+++ utils/kafka.go
@@ -0,0 +1,30 @@
+package utils
+
+import (
+	"context"
+	"go-nf/config"
+
+	"github.com/segmentio/kafka-go"
+)
+
+func KafkaConn(cfg config.KafkaConnCfg) *kafka.Conn {
+	conn, err := kafka.DialLeader(context.Background(), "tcp", cfg.Url, cfg.Topic, 0)
+	if err != nil {
+		panic(err.Error())
+	}
+	return conn
+}
+
+func IsTopicAlreadyExists(conn *kafka.Conn, topic string) bool {
+	partitions, err := conn.ReadPartitions()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	for _, p := range partitions {
+		if p.Topic == topic {
+			return true
+		}
+	}
+	return false
+}
