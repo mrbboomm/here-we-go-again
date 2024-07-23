1d9db0955206cdbef2b312c6b1f1619017989035
thitiwut.t <thitiwut.t@muze.co.th>
2024-07-23 11:57:21 +0700

    updated: consumer

--- kafka/consumer/main.go
+++ kafka/consumer/main.go
@@ -1,34 +1,72 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"go-nf/config"
-	"go-nf/utils"
+	"log"
+	"os"
 
 	"github.com/segmentio/kafka-go"
 )
 
 type ConsumerHandler struct {
-	Conn *kafka.Conn
+	BrokerAddress string
+	Topics        []string
+	Ctx           context.Context
 }
 
-func (c *ConsumerHandler) SubscribeEvent() {
+func (c *ConsumerHandler) InitializeReader() {
+	// Kafka broker address
+	brokerAddress := c.BrokerAddress
+
+	// Topics to subscribe to
+	topics := c.Topics
+
+	// Kafka reader configuration
+	readerConfig := kafka.ReaderConfig{
+		Brokers:  []string{brokerAddress},
+		GroupID:  "my-group",
+		MinBytes: 10e3, // 10KB
+		MaxBytes: 10e6, // 10MB
+	}
+
+	// Create Kafka readers for each topic
+	readers := make([]*kafka.Reader, len(topics))
+	for i, topic := range topics {
+		config := readerConfig
+		config.Topic = topic
+		readers[i] = kafka.NewReader(config)
+	}
+
+	defer c.CloseReader(readers)
+	// Consume messages from the topics
 	for {
-		message, err := c.Conn.ReadMessage(10e3)
-		if err != nil {
-			break
+		fmt.Printf("waiting message for %v reader \n", len(readers))
+		for _, reader := range readers {
+			msg, err := reader.ReadMessage(c.Ctx)
+			if err != nil {
+				log.Printf("Error while reading message: %v", err)
+				continue
+			}
+			fmt.Printf("Received message from %s: %s\n", msg.Topic, string(msg.Value))
+			fmt.Printf("message at offset %d: %s = %s\n", msg.Offset, string(msg.Key), string(msg.Value))
 		}
-		fmt.Println(string(message.Value))
+		// Add a small delay to avoid tight looping
+		// time.Sleep(1 * time.Second)
+	}
+}
+
+func (c *ConsumerHandler) CloseReader(readers []*kafka.Reader) {
+	// Close the readers when done
+	for _, reader := range readers {
+		if err := reader.Close(); err != nil {
+			log.Fatal("failed to close reader:", err)
+		}
 	}
 }
 
 func main() {
-	cfg := config.KafkaConnCfg{
-		Url:   "localhost:9092",
-		Topic: "tier",
-	}
-	conn := utils.KafkaConn(&cfg)
-
-	consumer := &ConsumerHandler{Conn: conn}
-	consumer.SubscribeEvent()
+	consumer := &ConsumerHandler{BrokerAddress: os.Getenv("KAFKA_HOST"), Topics: config.KafkaTopics, Ctx: context.Background()}
+	consumer.InitializeReader()
 }
