8ad63f7f3321e6261552d5ed09a09662e8bc5458
thitiwut.t <thitiwut.t@muze.co.th>
2024-07-23 11:55:58 +0700

    updated: producer

--- kafka/producer/producer.go
+++ kafka/producer/producer.go
@@ -1,39 +1,67 @@
 package producer
 
 import (
+	"context"
+	"fmt"
 	"go-nf/utils"
 	"log"
-	"time"
+	"os"
 
+	"github.com/gofiber/fiber/v2"
 	"github.com/segmentio/kafka-go"
 )
 
 type ProducerHandler struct {
-	Conn *kafka.Conn
+	Writer *kafka.Writer
+}
+
+type EventPayload struct {
+	Topic   string `json:"topic"`
+	Message any    `json:"message"`
+}
+
+// Initialize Producer
+func Initialize(topic string) *ProducerHandler {
+	brokerAddress := os.Getenv("KAFKA_HOST")
+	log.Printf("broker address %s with topic %s \n", brokerAddress, topic)
+
+	// init Writer
+	w := &kafka.Writer{
+		Addr:     kafka.TCP([]string{brokerAddress}...),
+		Balancer: &kafka.LeastBytes{},
+	}
+	return &ProducerHandler{Writer: w}
 }
 
 // Method PublishEvent
-func (p *ProducerHandler) PublishEvent(msgs ...any) {
+func (p *ProducerHandler) PublishEvent(payloads ...EventPayload) string {
 	// Convert into kafka.Message{}
 	// TODO move to utils
 	messages := make([]kafka.Message, 0)
-	for _, p := range msgs {
+	for _, payload := range payloads {
 		messages = append(messages, kafka.Message{
-			Value: utils.CompressToJsonBytes(&p),
+			Topic: payload.Topic,
+			Value: utils.CompressToJsonBytes(&payload.Message),
 		})
 	}
-
-	// Set timeout
-	p.Conn.SetReadDeadline(time.Now().Add(10 * time.Second))
-	_, err := p.Conn.WriteMessages(messages...)
+	ctx := context.Background()
+	err := p.Writer.WriteMessages(ctx, messages...)
 	if err != nil {
-		log.Fatal("failed to write messages:", err)
+		log.Printf("Failed to write message: %v", err)
 	}
-}
 
-// Method CloseConnection
-func (p *ProducerHandler) CloseConnection() {
-	if err := p.Conn.Close(); err != nil {
+	if err := p.Writer.Close(); err != nil {
 		log.Fatal("failed to close writer:", err)
 	}
+
+	fmt.Printf("Successfully produced message: %v\n", payloads)
+	return "Successfully produced message"
+}
+
+func SendMassage(c *fiber.Ctx) error {
+	payload := new(EventPayload)
+	c.BodyParser(payload)
+
+	writer := Initialize(payload.Topic)
+	return c.JSON(writer.PublishEvent([]EventPayload{*payload}...))
 }
