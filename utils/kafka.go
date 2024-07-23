2826ae56f7eeea26fdfc1c3e21b8ee258b85bccb
thitiwut.t <thitiwut.t@muze.co.th>
2024-07-23 11:57:07 +0700

    updated: kafka utils

--- utils/kafka.go
+++ utils/kafka.go
@@ -1,37 +1,57 @@
 package utils
 
 import (
-	"context"
 	"go-nf/config"
 	"log"
+	"net"
+	"strconv"
 
+	"github.com/gofiber/fiber/v2"
 	"github.com/segmentio/kafka-go"
 )
 
+type KafkaHandler struct {
+	Conn *kafka.Conn
+}
+
 // Create Connection
-func KafkaConn(cfg *config.KafkaConnCfg) *kafka.Conn {
-	conn, err := kafka.DialLeader(context.Background(), "tcp", cfg.Url, cfg.Topic, 0)
+func KafkaConn(cfg *config.KafkaConnCfg) *KafkaHandler {
+	conn, err := kafka.Dial("tcp", cfg.Url)
 	if err != nil {
 		panic(err.Error())
 	}
+	// defer conn.Close()
+	return &KafkaHandler{Conn: conn}
+}
+
+// Create Topic
+func CreateTopic(conn *kafka.Conn) {
+	controller, err := conn.Controller()
+	if err != nil {
+		panic(err.Error())
+	}
+	var controllerConn *kafka.Conn
+	controllerConn, err = kafka.Dial("tcp", net.JoinHostPort(controller.Host, strconv.Itoa(controller.Port)))
+	if err != nil {
+		panic(err.Error())
+	}
+	defer controllerConn.Close()
 
 	// Check topic if already exists or not
-	if !isTopicAlreadyExists(conn, cfg.Topic) {
-		topicConfigs := []kafka.TopicConfig{
-			{
-				Topic:             cfg.Topic,
+	var topicConfigs []kafka.TopicConfig
+	for _, topic := range config.KafkaTopics {
+		if !isTopicAlreadyExists(conn, topic) {
+			topicConfigs = append(topicConfigs, kafka.TopicConfig{
+				Topic:             topic,
 				NumPartitions:     1,
 				ReplicationFactor: 1,
-			},
-		}
-
-		err := conn.CreateTopics(topicConfigs...)
-		if err != nil {
-			panic(err.Error())
+			})
 		}
 	}
 
-	return conn
+	if err := controllerConn.CreateTopics(topicConfigs...); err != nil {
+		panic(err.Error())
+	}
 }
 
 // Close Connection
@@ -41,6 +61,7 @@ func CloseConnection(conn *kafka.Conn) {
 	}
 }
 
+// Validate Kafka Topic
 func isTopicAlreadyExists(conn *kafka.Conn, topic string) bool {
 	partitions, err := conn.ReadPartitions()
 	if err != nil {
@@ -53,4 +74,31 @@ func isTopicAlreadyExists(conn *kafka.Conn, topic string) bool {
 		}
 	}
 	return false
+}
+
+func ListTopic(conn *kafka.Conn) []string {
+	partitions, err := conn.ReadPartitions()
+	if err != nil {
+		panic(err.Error())
+	}
+	topics := []string{}
+	for i := 0; i < len(partitions); i++ {
+		topics = append(topics, partitions[i].Topic)
+	}
+	return topics
+}
+
+func (k *KafkaHandler) GetListTopic(c *fiber.Ctx) error {
+	topics := ListTopic(k.Conn)
+	return c.JSON(topics)
+}
+
+func (k *KafkaHandler) CreateTopics(c *fiber.Ctx) error {
+	CreateTopic(k.Conn)
+	return c.SendString("Created topics")
+}
+
+func (k *KafkaHandler) DeleteTopic(c *fiber.Ctx) error {
+	k.Conn.DeleteTopics(ListTopic(k.Conn)...)
+	return c.SendString("Deleted all topics")
 }
